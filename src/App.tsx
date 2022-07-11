1569934e6c2f5e988e87f184b43ff3d6423a6365
kathleen <kai.ling@besquare.com.my>
2022-07-11 10:13:55 +0800

    coded gender selection and clear btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { getValue } from "@testing-library/user-event/dist/utils";
 import * as React from "react";
+import { useRef } from "react";
+
 import "./App.css";
 
 export default function App() {
+  //[items inside the component state, fuction called to update the items]
+  //("default data")
   const [name, setName] = React.useState<string>("");
   const [age, setAge] = React.useState<string | undefined>("");
   const [color, setColor] = React.useState<string | undefined>("red");
+  const [gender, setGender] = React.useState<string>("Female");
 
   const changeName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
@@ -20,7 +26,18 @@ export default function App() {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(name, age, color);
+    console.log(name, age, gender, color);
+  };
+
+  const changeGender = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setGender(event.target.value);
+  };
+
+  const isRadioSelected = (value: string): boolean => gender === value;
+
+  const resetInputField = () => {
+    setName("");
+    setAge("");
   };
 
   return (
@@ -28,11 +45,38 @@ export default function App() {
       <form onSubmit={handleSubmit}>
         <Input name="Name" value={name} onChange={changeName} />
         <Input name="Age" value={age} onChange={changeAge} />
+        <div className="mb-16">
+          <label>Gender</label>
+          <div className="radiostyle">
+            <div>
+              <input
+                type="radio"
+                name="Gender"
+                value="Female"
+                onChange={changeGender}
+                checked={isRadioSelected("Female")}
+              />
+              <span>Female</span>
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="Gender"
+                value="Male"
+                onChange={changeGender}
+                checked={isRadioSelected("Male")}
+              />
+              <span> Male</span>
+            </div>
+          </div>
+        </div>
         <Select name="Favourite Color" value={color} onChange={changeColor} />
         <button type="submit" className="btn-primary mb-16">
           Submit
         </button>
-        <button className="btn-secondary">Clear</button>
+        <button onClick={resetInputField} className="btn-secondary">
+          Clear
+        </button>
       </form>
     </div>
   );
